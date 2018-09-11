d7f2a60101a4920a4f30cf7abc2ffda3b6b83b41
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-09-10 17:07:09 -0700

    This tells you which number is biggest.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -14,11 +14,15 @@ int main()
   //get second input using std::cin>> again with a second variable
     std::cin >> number2;
   //determine if first or second is bigger and store that in a third variable
-
-
+    int biggest;
+if(number1>number2) {
+   biggest = number1;
+} else {
+   biggest = number2;
+}
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
-
+  std::cout<<biggest<<std::endl;
   std::cout<<std::endl<<"Thank you for playing.\n";
   
   return 0;
